e9c93b80d6a65232e1eef5a41acf3ce3c54f5d8c
LufunoD10 <25090736@mvula.univen.ac.za>
2025-04-04 03:39:05 -0700

    Add files via upload
    first commit

---
+++ BurmaShaveJava.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.burmashave.java;
+
+/**
+ *
+ * @author 25090736
+ */
+import javax.swing.JOptionPane;
+public class BurmaShaveJava {
+
+    public static void main(String[] args) {
+     JOptionPane.showMessageDialog(null, "Shaving brushes ");
+     JOptionPane.showMessageDialog(null, "You'll soon see'em "); 
+     JOptionPane.showMessageDialog(null, "On a shelf "); 
+     JOptionPane.showMessageDialog(null, "In some museum"); 
+      
+      
+      
+    }
+}
